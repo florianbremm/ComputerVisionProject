759fe8737cf754f36ef71a64f88c26821d0af87b
Ein Kursaccount <cviwo013@ramses1.host>
2025-06-24 15:15:19 +0200

    updated CellDataset & load_pretrained

--- CellDataset.py
+++ CellDataset.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from torch.utils.data import Dataset
 from torchvision import transforms as T
 import json
+from typing import Tuple
 
 
 _json_path = Path('train_test_split.json')
@@ -14,6 +15,7 @@ with open(_json_path, 'r') as f:
 # Access the train and test entries
 train_list = _split_data.get("train", [])
 test_list = _split_data.get("test", [])
+val_list = _split_data.get("val", [])
 
 moco_transform = T.Compose([
     T.RandomHorizontalFlip(p=0.5),      # flip left-right with 50% probability
@@ -29,7 +31,7 @@ base_path = '/scratch/cv-course-group-5/data/dataset_jpg'
 dst_root   = Path(base_path + '/preprocessed_dataset')
 
 class CellDataset(Dataset):
-    def __init__(self, video_list=train_list, path_to_videos=dst_root, transform=None):
+    def __init__(self, video_list=train_list, path_to_videos=dst_root, transform=T.ToTensor()):
         self.image_paths = []
         self.path_to_videos = path_to_videos
         self.transform = transform
@@ -42,12 +44,14 @@ class CellDataset(Dataset):
         return len(self.image_paths)
 
     def __getitem__(self, idx):
+        """
+        :param idx: Inter
+        :return: Item at idx after applying transform
+        :rtype: Tuple[torch.Tensor]
+        """
         img_path = self.image_paths[idx]
         img = Image.open(img_path).convert("RGB")
 
-        if self.transform:
-            view1 = self.transform(img)
-            view2 = self.transform(img)
-            return view1, view2
-
-        return img
+        view1 = self.transform(img)
+        view2 = self.transform(img)
+        return view1, view2
