b07c8524d06e0f93f5af7e7777b4f4c753412bf0
florian.bremm@addmore.de <florian.bremm@alumni.fh-aachen.de>
2025-07-13 19:55:57 +0200

    refactored video lists

--- CellDataset.py
+++ CellDataset.py
@@ -9,7 +9,7 @@ import torch
 import lmdb
 
 
-_json_path = Path('train_test_split.json')
+_json_path = Path('video_lists/train_test_split.json')
 
 # Load the file
 with open(_json_path, 'r') as f:
@@ -20,7 +20,7 @@ train_list = _split_data.get("train", [])
 test_list = _split_data.get("test", [])
 val_list = _split_data.get("val", [])
 
-_reduced_path = Path('reduced_videos.json')
+_reduced_path = Path('video_lists/reduced_videos.json')
 with open(_reduced_path, 'r') as f:
     _reduced_list = json.load(f)
 
--- clustering.py
+++ clustering.py
@@ -180,7 +180,7 @@ if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     batch_size = 64
 
-    _json_path = Path('train_test_split.json')
+    _json_path = Path('video_lists/train_test_split.json')
 
     # Load the file
     with open(_json_path, 'r') as f:
